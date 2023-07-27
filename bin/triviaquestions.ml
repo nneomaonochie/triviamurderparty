6bf13c2538beac7060f990f9b5fbcb71e4e1bf86
Anoushka <anoushka.m.arora@gmail.com>
2023-07-27 15:45:34 +0000

    trivia question getters

---
+++ bin/triviaquestions.ml
@@ -0,0 +1,53 @@
+(*open! Core let question content : string = let open Soup in *)
+open! Core
+open! Yojson
+
+(* Module that will store questions that are randomly selected from JSON
+   file*)
+module Question = struct
+  type t =
+    { question : string
+    ; answer_choices : string list
+    ; correct_answer : string
+    }
+  [@@deriving compare, equal, sexp_of]
+end
+
+(* Converting JSON file into Yojson datatype*)
+let get_data_from_file filename = Yojson.Basic.from_file filename
+
+let get_question_array file_name =
+  let open Yojson.Basic.Util in
+  let json = get_data_from_file file_name in
+  json |> to_list
+;;
+
+let pick_random_question () : Question.t =
+  let open Yojson.Basic.Util in
+  let array =
+    get_question_array
+      "/usr/local/home/jsipuser/triviamurder/bin/trivia.json"
+  in
+  let array_size = List.length array in
+  let random_idx = Random.int array_size in
+  let elem = List.nth_exn array random_idx in
+  let q = elem |> member "question" |> to_string in
+  let a = elem |> member "A" |> to_string in
+  let b = elem |> member "B" |> to_string in
+  let c = elem |> member "C" |> to_string in
+  let d = elem |> member "D" |> to_string in
+  let ans_choices =
+    []
+    @ [ String.concat [ "A: "; a ] ]
+    @ [ String.concat [ "B: "; b ] ]
+    @ [ String.concat [ "C: "; c ] ]
+    @ [ String.concat [ "D: "; d ] ]
+  in
+  let c_ans = elem |> member "answer" |> to_string in
+  { question = q; answer_choices = ans_choices; correct_answer = c_ans }
+;;
+
+let print_random_question () =
+  let q = pick_random_question () in
+  print_s [%message "Random Question: " (q : Question.t)]
+;;
