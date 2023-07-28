88f8e5728923ae0283f0be5c817779a91c7782b4
Nneoma <nneoma.amy@gmail.com>
2023-07-28 19:29:38 +0000

    adding server files

--- lib/game_state.ml
+++ lib/game_state.ml
@@ -1,7 +1,6 @@
 open! Core
 
 type t =
-  (* should we make a Beginning module? *)
   | Ongoing
   | Game_over
 [@@deriving compare, equal, sexp_of]
---
+++ lib/tmp_server.ml
@@ -0,0 +1,11 @@
+[@@@disable_unused_warnings]
+
+open! Core
+open! Async
+module Server = struct end
+module Client = struct end
+
+module RPC = struct
+  (* we could use a RPC for asking a user's name (String), and getting a
+     user's answer (char) *)
+end
