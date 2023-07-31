2582828b0d2271be631da435d83a33c9f5f99217
Anoushka <anoushka.m.arora@gmail.com>
2023-07-31 15:51:28 +0000

    m

--- lib/tmp_server.ml
+++ lib/tmp_server.ml
@@ -2,10 +2,3 @@
 
 open! Core
 open! Async
-module Server = struct end
-module Client = struct end
-
-module RPC = struct
-  (* we could use a RPC for asking a user's name (String), and getting a
-     user's answer (char) *)
-end
