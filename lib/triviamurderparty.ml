26e3c2b6410fc7f60d243b263a0c6bd992f02885
Nneoma <nneoma.amy@gmail.com>
2023-07-31 20:37:54 +0000

    creating the beginning thing

--- lib/triviamurderparty.ml
+++ lib/triviamurderparty.ml
@@ -1,17 +1,11 @@
 open! Core
 open Async
 
-let main : unit -> unit Deferred.t =
- fun () ->
-  let _q = Triviaquestions.pick_random_question () in
-  return ()
-;;
+(* let main : unit -> unit Deferred.t = fun () -> let _q =
+   Triviaquestions.pick_random_question () in return () ;; *)
 
 let command =
-  (* Command.group ~summary:"TMP server" [ "tmp-server", Tmp_server.command
-     ] *)
-  Command.async
-    ~summary:"Start server for example [starter_template]"
-    (let%map_open.Command () = return () in
-     fun () -> main ())
+  Command.group ~summary:"TMP server" [ "tmp-server", Tmp_server.command ]
 ;;
+(* Command.async ~summary:"Start server for example [starter_template]"
+   (let%map_open.Command () = return () in fun () -> main ()) *)
