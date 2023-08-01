7b681784877b010baf78c31f1cd12ef7f36b17c2
Nneoma <nneoma.amy@gmail.com>
2023-08-01 21:16:30 +0000

    math game graphics

--- lib/tmp_graphics.ml
+++ lib/tmp_graphics.ml
@@ -1,6 +1,7 @@
 [@@@disable_unused_warnings]
 
 open! Core
+open! Async
 
 module Color = struct
   let black = Graphics.rgb 000 000 000
@@ -9,6 +10,8 @@ module Color = struct
   let blue = Graphics.rgb 000 000 255
 end
 
+(* for both width and heights of the players *)
+let player_block_size = 125 
 (* this asks user input for players' names so that we can initiatilize the
    players and create a game *)
 let create_players () = ()
@@ -16,8 +19,21 @@ let create_players () = ()
 (* display instructions for how TMP works after the game is created *)
 let display_beginning_instructions () = ()
 
+(* this will deal with the spacing *)
+let display_players players = 
+  (* spacing is based off of numPlayers *)
+  
+;;
+
 (* these are the graphics for specific game_kind*)
 let create_trivia_graphics () = ()
-let create_math_mayhem_graphics () = ()
+
+let create_math_mayhem_graphics
+  (participants : (Socket.Address.Inet.t * Player.t) list)
+  =
+  display_players participants; 
+  ()
+;;
+
 let create_decision_graphics () = ()
 let create_clicker_graphics () = ()
--- lib/tmp_graphics.mli
+++ lib/tmp_graphics.mli
@@ -1,3 +1,8 @@
 open! Core
+open! Async
 
 val create_players : unit -> unit
+
+val create_math_mayhem_graphics
+  :  (Socket.Address.Inet.t * Player.t) list
+  -> unit
