ee372ec7fda9b867db3af5adfa087dc34351ef1b
Nneoma <nneoma.amy@gmail.com>
2023-08-02 19:44:45 +0000

    updated display players

--- lib/tmp_graphics.ml
+++ lib/tmp_graphics.ml
@@ -38,18 +38,17 @@ let player_creation_screen () =
    number of players *)
 (* returns a list of players and their corresponding x_coord*)
 let display_players (players : (Socket.Address.Inet.t * Player.t) list)
-  : (Player.t * int) list
+  : ((Socket.Address.Inet.t * Player.t) * int) list
   =
-  let rec paste_players
-    x_coord
-    ~(players_left : Player.t list)
-    ~player_positions
-    : (Player.t * int) list
+  let rec paste_players x_coord ~players_left ~player_positions
+    : ((Socket.Address.Inet.t * Player.t) * int) list
     =
     if List.length players_left = 0
     then player_positions
     else (
-      let current_player = List.hd_exn players_left in
+      let (client, current_player) : Socket.Address.Inet.t * Player.t =
+        List.hd_exn players_left
+      in
       Graphics.set_color current_player.color;
       Graphics.fill_rect
         x_coord
@@ -64,7 +63,7 @@ let display_players (players : (Socket.Address.Inet.t * Player.t) list)
       (*this removes the player we just instantiated *)
       let players_left = List.tl_exn players_left in
       let player_positions =
-        player_positions @ [ current_player, x_coord ]
+        player_positions @ [ (client, current_player), x_coord ]
       in
       paste_players (x_coord + 250) ~players_left ~player_positions)
   in
@@ -72,7 +71,8 @@ let display_players (players : (Socket.Address.Inet.t * Player.t) list)
   then failwith "We need at least one player to display.";
   (* I am making it exclusively a list of players *)
   let display_player_list =
-    List.unzip players |> fun (_, player_list) -> player_list
+    players
+    (* List.unzip players |> fun (_, player_list) -> player_list *)
   in
   let num_players = List.length display_player_list in
   paste_players
@@ -85,13 +85,36 @@ let display_players (players : (Socket.Address.Inet.t * Player.t) list)
 let create_trivia_graphics () = ()
 let create_leaderboard_graphics (game : Game.t) = ()
 
+(* pastes the arithmetic stuff where they are supposed to be *)
+let paste_math_mayhem_qs
+  ((player, x_coord) : Player.t * int)
+  (correct_answers : (string, int) Base.Hashtbl.t)
+  =
+  Graphics.set_color Color.white;
+  Graphics.set_font "-*-fixed-medium-r-semicondensed--30-*-*-*-*-*-iso8859-1";
+  Graphics.moveto x_coord (player_block_size - 100);
+  let ques, ans = Math_mayhem.get_questions () in
+  Graphics.draw_string ques
+;;
+
+(* draws the string but what is the answer? how about we create a map with
+   client -> player -> score we need *)
+
 let create_math_mayhem_graphics
   (participants : (Socket.Address.Inet.t * Player.t) list)
   =
   (* we only want to send the list of players to the clients, and we do not
      want to include client *)
   let player_positions = display_players participants in
-  ()
+  (* mapping IP adress (client) -> correct (int) answer *)
+  let correct_answers : (string, int) Base.Hashtbl.t =
+    Hashtbl.create
+      ~growth_allowed:false
+      ~size:(List.length player_positions)
+      (module String)
+  in
+  (* the first questions are displayed *)
+  List.iter player_positions ~f:paste_math_mayhem_qs correct_answers
 ;;
 
 let create_decision_graphics () = ()
