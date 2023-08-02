cc2d3b7658e349540a678b105d1d2e43c1fe166d
Nneoma <nneoma.amy@gmail.com>
2023-08-02 16:43:30 +0000

    stuff

--- lib/color.ml
+++ lib/color.ml
@@ -6,6 +6,8 @@ module Color = struct
   let green = Graphics.rgb 000 255 000
   let red = Graphics.rgb 255 000 000
   let blue = Graphics.rgb 000 000 255
+  let dark_blue = Graphics.rgb 000 000 204
+  let dark_red = Graphics.rgb 168 41 41
 
   let random () =
     Graphics.rgb (Random.int 256) (Random.int 256) (Random.int 256)
