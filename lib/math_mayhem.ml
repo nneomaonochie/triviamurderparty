9cd9f6f66bf79fb71e9d33d4e2d83425d48ccad8
Nneoma <nneoma.amy@gmail.com>
2023-08-02 19:44:45 +0000

    updated display players

--- lib/math_mayhem.ml
+++ lib/math_mayhem.ml
@@ -16,36 +16,35 @@ open! Async
 (* the Math mayhem in graphics is the main one and it calls methods from
    here *)
 let get_questions () =
-  (* this will probably be in a loop *)
-  let operator = if Random.int 2 % 2 = 0 then " + " else " - " in
-  let first = Random.int 41 - 20 in
+  let operator_str = if Random.int 2 % 2 = 0 then " + " else " - " in
   (* random number from -20 to 20 *)
+  let first = Random.int 41 - 20 in
   let second = Random.int 41 - 20 in
   let correct_answer =
     first
     +
-    if String.equal operator " + "
+    if String.equal operator_str " + "
     then second
     else
       second * -1 (* if the operand is subtract, then we are subtracting *)
   in
   let question_string =
     (* the question we will show to the client *)
-    Int.to_string first ^ operator ^ Int.to_string second
+    Int.to_string first ^ operator_str ^ Int.to_string second
   in
-  (* this will probably be the end of the loop *)
-
-  (* 3. display question to player - this will utilize Protocol.Response.t *)
-  (* Graphics => Graphics 4 quadrants for each players each time i return a
-     question stirng -> if they get get correct answer, new string show
-     everything a player need to look at *)
-
-  (* 4. get player responses - each correct answer is +25 to score *)
-  (* wrong answers - in OG game they get stunned for X secs, may just
-     implement a $$ deduction instead *)
-  (* 5. keep track of score - lowest score dies *)
-
-  (* 6. [do last] the case where only one player is sent - get other players
-     to play or that they must reach 20 correct Q's *)
-  ()
+  question_string, correct_answer
 ;;
+(* this will probably be the end of the loop *)
+
+(* 3. display question to player - this will utilize Protocol.Response.t *)
+(* Graphics => Graphics 4 quadrants for each players each time i return a
+   question stirng -> if they get get correct answer, new string show
+   everything a player need to look at *)
+
+(* 4. get player responses - each correct answer is +25 to score *)
+(* wrong answers - in OG game they get stunned for X secs, may just implement
+   a $$ deduction instead *)
+(* 5. keep track of score - lowest score dies *)
+
+(* 6. [do last] the case where only one player is sent - get other players to
+   play or that they must reach 20 correct Q's *)
