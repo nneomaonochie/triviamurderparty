9736f680741239a975f69fb9989fc32434c7684e
Nneoma <nneoma.amy@gmail.com>
2023-08-10 20:38:47 +0000

    style

--- lib/triviamurderparty.ml
+++ lib/triviamurderparty.ml
@@ -4,5 +4,3 @@ open Async
 let command =
   Command.group ~summary:"TMP server" [ "tmp-server", Tmp_server.command ]
 ;;
-(* Command.async ~summary:"Start server for example [starter_template]"
-   (let%map_open.Command () = return () in fun () -> main ()) *)
